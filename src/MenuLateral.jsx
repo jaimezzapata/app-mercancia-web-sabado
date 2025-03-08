0702151a5c61bae58ecd0304ad7fcaf284148f7c
jaimezzapata <zapataval2304@gmail.com>
2025-03-08 17:10:18 -0500

    Introducción a las props

--- src/MenuLateral.jsx
+++ src/MenuLateral.jsx
@@ -1,15 +1,17 @@
 import userImg from './assets/user.jpg'
 function MenuLateral() {
+  let enlaces = ["Inicio", "Servicios", "Contacto", "Productos", "Acerca de", "Trabaja con nsotros"]
   return (
     <aside className="panel-control-aside">
       <div>
         <img src={userImg} alt="" />
       </div>
       <nav>
-        <a href="">Enlace</a>
-        <a href="">Enlace</a>
-        <a href="">Enlace</a>
-        <a href="">Enlace</a>
+        {
+          enlaces.map((enlace) => {
+            return <a href="">{enlace}</a>
+          })
+        }
       </nav>
     </aside>
   )
